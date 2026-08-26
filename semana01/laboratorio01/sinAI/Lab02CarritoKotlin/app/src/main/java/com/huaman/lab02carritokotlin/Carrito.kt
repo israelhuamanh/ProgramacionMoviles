690cb41c9d3cd9f19818c1575751b996b1769571
Israel Angel Huaman Huaman <izzzaaannngel@gmail.com>
2026-08-26 12:35:22 -0500

    Proyecto inicial con menu del carrit

---
+++ semana01/laboratorio01/sinAI/Lab02CarritoKotlin/app/src/main/java/com/huaman/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,7 @@
+package com.huaman.lab02carritokotlin
+
+fun main() {
+    println("=========================================")
+    println("    CARRITO DE COMPRAS - TIENDA TECSUP")
+    println("=========================================")
+}
