4bb1bb18ca514007fc7fb285398066c11811afee
Israel Huaman <israelhuamanh@gmail.com>
2026-09-16 17:29:13 +0000

    empezando proyecto con poo

---
+++ semana04/Laboratorio04/conIA/app/src/main/java/com/example/controltareas/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.controltareas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
