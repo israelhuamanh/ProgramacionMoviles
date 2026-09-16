939883f0aa033c332e6e58fa5bf20b5df7923598
Israel Huaman <israelhuamanh@gmail.com>
2026-09-16 17:47:15 +0000

    terminando el ui de la aplicacion y uniendo

--- semana04/Laboratorio04/conIA/app/build.gradle.kts
+++ semana04/Laboratorio04/conIA/app/build.gradle.kts
@@ -55,3 +55,5 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 }
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
